8ebb6b1ec2dc42bc372089c9d88655792ef311d6
marie135 <marieolbrich@web.de>
2019-03-04 12:16:01 +0100

    ++, -- und / funktionen eingefügt

--- emBOvoterPI.cpp
+++ emBOvoterPI.cpp
@@ -5,12 +5,18 @@
 #include "Mini-Log/miniLog.hpp"
 
 using namespace std::literals::chrono_literals;
-#define GREATPIN 27
-#define BADPIN 25
+#define GREATPIN 11
+#define BADPIN 13
+#define DOUBLEGREATPIN 15
+#define DOUBLEBADPIN 16
+#define MEDIUMPIN 18
 constexpr auto debounce_time = 20ms;
 
 static volatile int greatstate;
 static volatile int badstate;
+static volatile int doublegreatstate;
+static volatile int doublebadstate;
+static volatile int mediumstate;
 static auto last_change = std::chrono::system_clock::now();
 const std::string log_path {"./emBOvotes"};
 
@@ -33,7 +39,7 @@ void great(void) {
     std::chrono::duration<double> elapsed_since_last_change = now-last_change;
     if ( elapsed_since_last_change > debounce_time) {
         PrintTime();
-	std::cout << "This was good!\n";
+	std::cout << "+";
         ballotBox << goodLog;
 	greatstate = !greatstate;
     }
@@ -46,9 +52,48 @@ void bad(void) {
     std::chrono::duration<double> elapsed_since_last_change = now-last_change;
     if ( elapsed_since_last_change > debounce_time) {
         PrintTime();
-	std::cout << "This was bad!\n";
+	std::cout << "-";
         ballotBox << badLog;
 	badstate = !badstate;
+    }
+
+    last_change = now;
+}
+
+void doublebad(void) {
+    auto now = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_since_last_change = now-last_change;
+    if ( elapsed_since_last_change > debounce_time) {
+        PrintTime();
+	std::cout << "--";
+        ballotBox << badLog;
+	doublebadstate = !doublebadstate;
+    }
+
+    last_change = now;
+}
+
+void doublegreat(void) {
+    auto now = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_since_last_change = now-last_change;
+    if ( elapsed_since_last_change > debounce_time) {
+        PrintTime();
+	std::cout << "++";
+        ballotBox << badLog;
+	doublegreatstate = !doublegreatstate;
+    }
+
+    last_change = now;
+}
+
+void medium(void) {
+    auto now = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_since_last_change = now-last_change;
+    if ( elapsed_since_last_change > debounce_time) {
+        PrintTime();
+	std::cout << "/";
+        ballotBox << badLog;
+	mediumstate = !mediumstate;
     }
 
     last_change = now;
@@ -58,10 +103,19 @@ void activateInterrupts(){
 	try{
 		pinMode(GREATPIN,	OUTPUT);
 		pinMode(BADPIN	,	OUTPUT);
+		pinMode(DOUBLEGREATPIN	,	OUTPUT);
+		pinMode(DOUBLEBADPIN	,	OUTPUT);
+		pinMode(MEDIUMPIN	,	OUTPUT);
 		wiringPiISR(GREATPIN,	INT_EDGE_FALLING,	&great);
 		wiringPiISR(BADPIN,	INT_EDGE_FALLING,	&bad);
+		wiringPiISR(DOUBLEGREATPIN,	INT_EDGE_FALLING,	&doublegreat);
+		wiringPiISR(DOUBLEBADPIN,	INT_EDGE_FALLING,	&doublebad);
+		wiringPiISR(MEDIUMPIN,	INT_EDGE_FALLING,	&medium);
 		greatstate	=	digitalRead(GREATPIN);
 		badstate	=	digitalRead(BADPIN);
+		doublegreatstate	=	digitalRead(DOUBLEGREATPIN);
+		doublebadstate	=	digitalRead(DOUBLEBADPIN);
+		mediumstate	=	digitalRead(MEDIUMPIN);
 	}
 	catch(const std::exception& e){
 		std::cout << " activating Interrupts caught a std:exception, with message '"
@@ -79,8 +133,11 @@ struct MillisecondTimer_t{
 
 void initMessage(){
 	std::cout << 	"emBO++ Votersystem successfully launched!" << std::endl <<
-			"initial greatstate is " << greatstate << std::endl << 
-			"initial  badstate  is " << badstate << std::endl;
+			"initial doublegreatstate is " << doublegreatstate << std::endl << 
+			"initial  greatstate  is " << greatstate << std::endl
+			"initial  mediumstate  is " << mediumstate << std::endl
+			"initial  badstate  is " << badstate << std::endl
+			"initial  doublebadstate  is " << doublebadstate << std::endl;
 }
 
 int main(void) {
